017d38106f304620aebe54177668198c8acc7c07
Eldelaltillo <edward_villa_9860@hotmail.com>
2026-04-17 16:36:46 -0500

    Agrego estructura completa del portafolio con CSS y JS

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
-console.log("Hola desde JavaScript 🚀");
+// SCROLL SUAVE
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const target = document.querySelector(this.getAttribute('href'));
+    target.scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+
+// CAMBIAR NAVBAR AL HACER SCROLL
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    header.style.background = "rgba(2, 6, 23, 0.95)";
+  } else {
+    header.style.background = "rgba(15, 23, 42, 0.9)";
+  }
+});
+
+
+// ANIMACIÓN AL HACER SCROLL
+const sections = document.querySelectorAll("section");
+
+const revealOnScroll = () => {
+  const triggerBottom = window.innerHeight * 0.85;
+
+  sections.forEach(section => {
+    const sectionTop = section.getBoundingClientRect().top;
+
+    if (sectionTop < triggerBottom) {
+      section.style.opacity = "1";
+      section.style.transform = "translateY(0)";
+    }
+  });
+};
+
+// Estado inicial (oculto)
+sections.forEach(section => {
+  section.style.opacity = "0";
+  section.style.transform = "translateY(50px)";
+  section.style.transition = "all 0.6s ease";
+});
+
+window.addEventListener("scroll", revealOnScroll);
+window.addEventListener("load", revealOnScroll);
